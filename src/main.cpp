290b7db8549577194c36b6845124791cb5387e0c
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-12-09 09:02:38 -0800

    Added RANSAC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <iostream>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/core/base.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/features2d.hpp>
@@ -9,8 +11,46 @@
 #include <opencv2/videoio.hpp>
 
 using namespace cv;
-using std::cout, std::endl, std::cerr;
-using std::vector;
+using std::cout, std::endl, std::cerr, std::vector;
+
+Mat processFrame(Mat& frame1, Mat& frame2) {
+  Mat resFrame, gray1, gray2;
+
+  cvtColor(frame1, gray1, COLOR_BGR2GRAY);
+  cvtColor(frame2, gray2, COLOR_BGR2GRAY);
+
+  Ptr< ORB > orb = orb->create();
+  vector< KeyPoint > keypoints1, keypoints2;
+  Mat descriptor1, descriptor2;
+
+  orb->detectAndCompute(gray1, noArray(), keypoints1, descriptor1);
+  orb->detectAndCompute(gray2, noArray(), keypoints2, descriptor2);
+
+  BFMatcher bfMatcher;
+  bfMatcher.create(NORM_HAMMING, true);
+
+  vector< DMatch > matches;
+  bfMatcher.match(descriptor1, descriptor2, matches);
+
+  vector< DMatch > inlierMatches;
+  vector< Point2f > points1, points2;
+  for(const auto& match : matches) {
+    points1.emplace_back(keypoints1[match.queryIdx].pt);
+    points2.emplace_back(keypoints2[match.trainIdx].pt);
+  }
+
+  vector< uchar > inlierMask;
+  findFundamentalMat(points1, points2, FM_RANSAC, 3, 0.99, inlierMask);
+  for(int i = 0; i < inlierMask.size(); ++i) {
+    if(inlierMask[i]) {
+      inlierMatches.emplace_back(matches[i]);
+    }
+  }
+
+  drawMatches(frame1, keypoints1, frame2, keypoints2, matches, resFrame);
+
+  return resFrame;
+}
 
 int main() {
   std::string video_path
@@ -25,28 +65,20 @@ int main() {
   double fps = 0;
   std::chrono::time_point< std::chrono::high_resolution_clock > start, end;
 
-  Mat frame, grayFrame;
-  int maxCorners = 100;
-  double qualityLevel = 0.01;
-  double minDistance = 10.0;
+  Mat currentFrame, previousFrame;
 
-  while(cap.read(frame)) {
+  cap >> previousFrame;
+
+  while(cap.read(currentFrame)) {
     start = std::chrono::high_resolution_clock::now();
 
-    cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
-
-    Ptr< ORB > orb = ORB::create();
-    vector< KeyPoint > keypoints;
-    Mat descriptors;
-
-    orb->detectAndCompute(grayFrame, noArray(), keypoints, descriptors);
-    drawKeypoints(frame, keypoints, frame);
+    Mat resFrame = processFrame(previousFrame, currentFrame);
 
     end = std::chrono::high_resolution_clock::now();
     double elapsed = std::chrono::duration< double >(end - start).count();
     fps = 1.0 / elapsed;
 
-    cv::putText(frame,
+    cv::putText(resFrame,
                 "FPS: " + std::to_string(fps),
                 cv::Point(10, 30),
                 cv::FONT_HERSHEY_SIMPLEX,
@@ -54,11 +86,13 @@ int main() {
                 Scalar(0, 255, 0),
                 2);
 
-    imshow("Live", frame);
+    imshow("Live", resFrame);
 
     if(waitKey(5) >= 0) {
       break;
     }
+
+    previousFrame = currentFrame;
   }
 
   return 0;
